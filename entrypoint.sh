fb08f62a0d94bded71839b770e3392385c06feea
sean cavanaugh <sean@cavanaugh.pro>
2022-09-19 12:49:01 -0400

    sync

--- entrypoint.sh
+++ entrypoint.sh
@@ -80,7 +80,8 @@ tee playbook.yml << EOF
         extra_vars: "$EXTRA_VARS"
         scm_url: "$scm_url"
         scm_branch: "$scm_branch"
-        scm_refspec: "$scm_refspec"
+        # scm_refspec: "$scm_refspec"
+        scm_refspec: "refs/pull/*/head:refs/remotes/origin/pull/*"
 
     - name: print out extra_vars
       debug:
@@ -102,7 +103,7 @@ tee playbook.yml << EOF
             - scm_type: "{{ project_info[0].scm_type }}"
             - scm_url: "{{ project_info[0].scm_url }}"
             - scm_branch: "{{ project_info[0].scm_branch }}"
-            - scm_refspec: "refs/pull/*/head:refs/remotes/origin/pull/*"
+            - scm_refspec: "{{ project_info[0].scm_refspec }}"
             - credential: "{{ project_info[0].credential }}"
             - scm_clean: "{{ project_info[0].scm_clean }}"
             - scm_delete_on_update: "{{ project_info[0].scm_delete_on_update }}"
@@ -120,7 +121,7 @@ tee playbook.yml << EOF
           scm_type: "{{ project_info[0].scm_type }}"
           scm_url: "$scm_url"
           scm_branch: "{{ project_info[0].scm_branch }}"
-          scm_refspec: "$scm_refspec"
+          scm_refspec: "{{ scm_refspec }}"
           # credential: "{{ project_info[0].credential }}"
           scm_clean: "{{ project_info[0].scm_clean }}"
           scm_delete_on_update: "{{ project_info[0].scm_delete_on_update }}"
