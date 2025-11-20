ef3db7171420e5941cfe89dee414c40bab76942e
Alex Walczyk <abwalczyk@gmail.com>
2025-11-20 11:00:05 -0500

    Updating entrypoint.sh community.general

--- entrypoint.sh
+++ entrypoint.sh
@@ -58,7 +58,8 @@ echo "scm_branch is $scm_branch"
 tee ansible.cfg << EOF
 [defaults]
 COLLECTIONS_PATHS = /root/.ansible/collections
-stdout_callback=community.general.yaml
+stdout_callback = default
+callback_result_format = yaml
 callbacks_enabled=ansible.posix.profile_tasks
 
 EOF
